e4601c697e0a72c82784c27dec6aff74a456414f
dirk <dirkadin@gmail.com>
2018-07-02 20:35:34 -0500

    Reworked translate method to use mark methods correctly Added some javadocs

--- Translator.java
+++ Translator.java
@@ -11,6 +11,12 @@ public class Translator {
     private OutputBuffer output;
 
 
+    /**
+     * Creates a instance of the translator class.
+     * The translator is where the bulk of the work happens.
+     *
+     * @param inputFile The filename of the file you wish to translate.
+     */
     public Translator(String inputFile) {
 
         // Determining if we are translating to morse or not
@@ -40,9 +46,24 @@ public class Translator {
         }
     }
 
+    /**
+     * Handles the translation of files.  Input and output.
+     */
     public void translate() {
+        output.markEndOfSentence();
         do {
-            output.putChar(input.getChar().convert());
+
+            String stringInput = input.getChar().convert();
+
+            if (stringInput.equals(" ")) {
+                output.markEndOfWord();
+            } else if (stringInput.equals(System.lineSeparator())) {
+                output.markEndOfSentence();
+            } else {
+                output.putChar(stringInput);
+            }
+
+//            output.putChar(input.getChar().convert());
         } while (!input.endOfBuffer());
 
         input.close();
